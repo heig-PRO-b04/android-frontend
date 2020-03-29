8bdcc6e610338999d5d7518a44f7003154381a97
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-03-29 16:29:54 +0200

    Add Button to Home activity

--- app/src/main/java/ch/heigvd/pro/b04/android/Home.java
+++ app/src/main/java/ch/heigvd/pro/b04/android/Home.java
@@ -2,7 +2,10 @@ package ch.heigvd.pro.b04.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the user taps the Send button
+     * @param view
+     */
+    public void scanQR(View view) {
+        //TODO voir comment récupérer le code QR et le passer au serveur ou je sais pas quoi
+        try {
+            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+            intent.putExtra("SCAN_MODE", "QR_CODE_MODE"); // "PRODUCT_MODE for bar codes
+
+            startActivityForResult(intent, 0);
+        } catch (Exception e) {
+            Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
+            Intent marketIntent = new Intent(Intent.ACTION_VIEW,marketUri);
+            startActivity(marketIntent);
+        }
     }
 }
