c71e7240e406ab10de84e5fe054e39ecdf52ee95
Clarisse Fleurimont <clarisse.fleu@gmail.com>
2020-04-06 09:12:12 +0200

    Add missing emojis

--- app/src/main/java/ch/heigvd/pro/b04/android/Home/Emoji.java
+++ app/src/main/java/ch/heigvd/pro/b04/android/Home/Emoji.java
@@ -5,7 +5,7 @@ public class Emoji {
             new Emoji("\u2705", '0'),
             new Emoji("\uD83C\uDF7A", '1'),
             new Emoji("\uD83C\uDF54", '2'),
-            new Emoji("\u2705", '3'),
+            new Emoji("\uD83D\uDE3B", '3'),
             new Emoji("\uD83D\uDC7B", '4'),
             new Emoji("\uD83E\uDD84", '5'),
             new Emoji("\uD83C\uDF40", '6'),
@@ -14,10 +14,10 @@ public class Emoji {
             new Emoji("\uD83E\uDD73", '9'),
             new Emoji("\uD83E\uDD51", 'A'),
             new Emoji("\uD83E\uDD76", 'B'),
-            new Emoji("\uD83E\uDD76", 'C'),
+            new Emoji("\uD83C\uDF8B", 'C'),
             new Emoji("\uD83C\uDF08", 'D'),
             new Emoji("\u2614", 'E'),
-            new Emoji("\u2614", 'F'),
+            new Emoji("\uD83C\uDFB9", 'F'),
     };
 
     private CharSequence emoji;
