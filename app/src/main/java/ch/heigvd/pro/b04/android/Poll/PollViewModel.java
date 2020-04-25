51d35395eba3c2be885f32575f861af3fe9ae221
ClarFleu <clarisse.fleu@gmail.com>
2020-04-25 11:39:20 +0200

    Add Session object to network API

--- app/src/main/java/ch/heigvd/pro/b04/android/Poll/PollViewModel.java
+++ app/src/main/java/ch/heigvd/pro/b04/android/Poll/PollViewModel.java
@@ -1,16 +1,45 @@
 package ch.heigvd.pro.b04.android.Poll;
 
+import android.util.Log;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
 import ch.heigvd.pro.b04.android.Poll.Question.Question;
+import ch.heigvd.pro.b04.android.datamodel.Session;
+import ch.heigvd.pro.b04.android.datamodel.Token;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class PollViewModel extends ViewModel {
     private MutableLiveData<Question> questionToView = new MutableLiveData<>();
     private MutableLiveData<List<Question>> questions = new MutableLiveData<>(new LinkedList<>());
+
+    private Callback<Session> callbackSession = new Callback<Session>() {
+        @Override
+        public void onResponse(Call<Session> call, Response<Session> response) {
+            if (response.isSuccessful()) {
+                // TODO : store poll info pollTitle.postValue(response.body());
+            } else {
+                Log.w("localDebug", "Received error, HTTP status is " + response.code());
+                try {
+                    Log.w("localDebug", response.errorBody().string());
+                } catch (IOException e) {
+                    Log.e("localDebug", "Error in error, rip");
+                }
+            }
+        }
+
+        @Override
+        public void onFailure(Call<Session> call, Throwable t) {
+            Log.e("localDebug", "We had a super bad error in callbackToken");
+        }
+    };
 
     public PollViewModel() {}
 
--- app/src/main/java/ch/heigvd/pro/b04/android/network/RockinAPI.java
+++ app/src/main/java/ch/heigvd/pro/b04/android/network/RockinAPI.java
@@ -1,13 +1,18 @@
 package ch.heigvd.pro.b04.android.network;
 
+import ch.heigvd.pro.b04.android.datamodel.Session;
 import ch.heigvd.pro.b04.android.datamodel.SessionCode;
 import ch.heigvd.pro.b04.android.datamodel.Token;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface RockinAPI {
 
     @POST("/connect")
     Call<Token> postConnect(@Body SessionCode code);
+
+    @GET("/session")
+    Call<Session> getSession(@Body Token userToken);
 }
