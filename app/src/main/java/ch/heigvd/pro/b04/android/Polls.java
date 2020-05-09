62b6f52ca41eabcf991ee0ce57cb564af6233cad
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-05-09 17:32:20 +0200

    Add QR code reading

--- app/src/main/java/ch/heigvd/pro/b04/android/Polls.java
+++ app/src/main/java/ch/heigvd/pro/b04/android/Polls.java
@@ -2,17 +2,11 @@ package ch.heigvd.pro.b04.android;
 
 import android.app.Application;
 
-import androidx.emoji.bundled.BundledEmojiCompatConfig;
-import androidx.emoji.text.EmojiCompat;
-
 public class Polls extends Application {
 
     @Override
     public void onCreate() {
 
         super.onCreate();
-
-        BundledEmojiCompatConfig config = new BundledEmojiCompatConfig(this);
-        EmojiCompat.init(config);
     }
 }
