4e40fda96205d95753b0968da464006082c46092
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-05-28 10:55:35 +0200

    Add comments

--- app/src/main/java/ch/heigvd/pro/b04/android/authentication/TokenToTokenWithSession.java
+++ app/src/main/java/ch/heigvd/pro/b04/android/authentication/TokenToTokenWithSession.java
@@ -12,6 +12,9 @@ import ch.heigvd.pro.b04.android.datamodel.Session;
 import ch.heigvd.pro.b04.android.network.LiveDataUtils;
 import ch.heigvd.pro.b04.android.network.Rockin;
 
+/**
+ * Maps a certain token to the associated {@link Session}.
+ */
 public class TokenToTokenWithSession implements Function<String, LiveData<Pair<String, Session>>> {
 
     @Override
