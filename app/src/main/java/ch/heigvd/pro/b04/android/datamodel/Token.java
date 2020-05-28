70950afe9052a88afb5b883df266145d372808fe
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-05-28 11:19:03 +0200

    Remove unused code

--- app/src/main/java/ch/heigvd/pro/b04/android/datamodel/Token.java
+++ app/src/main/java/ch/heigvd/pro/b04/android/datamodel/Token.java
@@ -6,10 +6,6 @@ public class Token {
     @SerializedName("token")
     private String token;
 
-    public Token(String token) {
-        this.token = token;
-    }
-
     public String getToken() {
         return token;
     }
